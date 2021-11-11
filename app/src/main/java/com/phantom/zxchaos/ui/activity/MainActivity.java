071b9e7cbb3ccd1bfb7035e448dfb028d76706c4
haoren84 <37094527+haoren84@users.noreply.github.com>
2021-11-11 16:10:29 +0800

    first activity

---
+++ app/src/main/java/com/phantom/zxchaos/ui/activity/MainActivity.java
@@ -0,0 +1,16 @@
+package com.phantom.zxchaos.ui.activity;
+
+import com.phantom.zxchaos.base.BaseActivity;
+
+import android.os.Bundle;
+
+import com.phantom.zxchaos.R;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
